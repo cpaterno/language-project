0dcd05df75306e0fc67ff56f6426999c9a272ec0
Vittorio Papandrea <papandreavittorio@gmail.com>
2017-11-18 19:18:35 -0500

    speculating finish of header file

--- language.h
+++ language.h
@@ -11,14 +11,34 @@
 
 class language {
 	public:
+		// Test constructor, calculates frequency of given "text"
 		language(std::string name, std::string text);
+
+		// Main constructor, takes in fileName and computes the trigram frequency of the file text.
 		language(std::string fileName);
 
+		// Gets the name of the language
 		std::string getName();
 
+		// Gets the frequency map
+		std::map<int, int> getFrequencyMap();
+
+		// Calculates cosine similarity of current language and given language.
+		double computeComparison(language toCompare);
+		
+		// Prints the frequency
+		void printFrequency();		
+
 	private:
+		// Helper function that updates the local frequency based on currently read in buffer.
+		void updateFrequency(*vector<char>);
+
+		// The trigram frequency map
 		std::map<int, int> trigrameFrequency;
-		std::string name; 
+		
+		// The name
+		std::string name;
+ 
 }	
 
 #endif
