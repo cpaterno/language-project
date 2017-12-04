6245a5d19715324cfb996cf8bc1e19bdb0872403
Vittorio Papandrea <papandreavittorio@gmail.com>
2017-12-04 17:34:17 -0500

    commented main2

--- main2.cpp
+++ main2.cpp
@@ -20,11 +20,18 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
+/*
+ * Finds the best match between one language and a list of other languages.
+ *
+ * @param v Pointer to vector of training languages
+ * @param test Pointer to language class that needs to be compared to the trianing data.
+ */
 language* findBestMatch(std::vector<language*> *v, language *test) {
 	double max = 0.0;
 	double bestLanguage = 0;
 	double tempComparison = 0.0;
 
+	// Glorified max function.
 	for (int languageIndex = 0; languageIndex < v->size(); languageIndex ++) {
 		tempComparison = v->at(languageIndex)->computeComparison(test);
 		if (tempComparison > max) {
